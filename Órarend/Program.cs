c4a0e3fac74cbcda7c4169884cd72e88cd02e0dd
Paplukács Levente <b2paplev@vasvari.org>
2024-09-13 15:11:52 +0200

    2024.09.13

--- Órarend/Program.cs
+++ Órarend/Program.cs
@@ -22,14 +22,38 @@ namespace Órarend
 
             for (int i = 0; i < dayCount; i++)//napok váltakozása
             {
+                int flip = 2;//1 első óra nincs; 2 utolsó ora nincs; 3 normál nap
                 Console.Write($"Hány órád legyen {days[i]}: ");
                 int lesson = Convert.ToInt32(Console.ReadLine());
+                if (flip == 1)
+                {
+                    Console.WriteLine($"1. -");
+                }
+                else if (flip == 2)
+                {
+                    Console.WriteLine($"{lesson}. -");
+                }
 
                 for (int counter = 0; counter < lesson; counter++)//Órák váltakozása
                 {
-                    lessonCounter++
-                    int index = random.Next(0, lessons.Length);
-                    Console.WriteLine($"{counter + 1}.{lessons[index]}");
+                    if (flip == 1)
+                    {
+                        int index = random.Next(0, lessons.Length);
+                        Console.WriteLine($"{counter + 1}.{lessons[index]}");
+                    }
+                    else if (flip == 2)
+                    {
+                        int index = random.Next(0, lessons.Length);
+                        Console.WriteLine($"{counter + 1}.{lessons[index]}");
+                    }
+                    else
+                    {
+                        int index = random.Next(0, lessons.Length);
+                        Console.WriteLine($"{counter + 1}.{lessons[index]}");
+
+                    }
+
+                    lessonCounter++;
                 }
             }
             Console.WriteLine($"Összesen {lessonCounter}db órád volt.");
