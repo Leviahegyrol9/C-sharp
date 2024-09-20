4e4d9e1eb9a0264d92d30cf26fd2d006ef00ce8c
Paplukács Levente <b2paplev@vasvari.org>
2024-09-20 10:26:22 +0200

    2024.09.20

--- Program/Program.cs
+++ Program/Program.cs
@@ -16,6 +16,7 @@ namespace Program
             int student = random.Next(17,28);
             double all = 0;
 
+            Console.WriteLine($"Az osztály létszáma: {student}");
 
             for (int i = 0; i < 10; i++)
             {
@@ -23,12 +24,39 @@ namespace Program
                 string program = Console.ReadLine();
                 programs.Add(program);
             }
+
+            Console.Clear();
+
             for (int i = 0; i < 10; i++)
             {
-                Console.Write($"Kérem a program összegét ({programs[i]}): ");
-                double money = Convert.ToDouble(Console.ReadLine());
-                all += money;
+                int number = 0;
+                bool isNumber = false;
+
+                do
+                {
+                    Console.Write($"Kérem a program összegét ({programs[i]}): ");
+                    string input = Console.ReadLine();
+                    isNumber = int.TryParse(input, out number);
+
+                    if (!isNumber)
+                    {
+                        Console.WriteLine("Nem számot adott meg!");
+                        Thread.Sleep(2500);
+                        Console.Clear();
+                    }
+                    else
+                    {
+                        double money = Convert.ToDouble(input);
+                        all += money;
+                    }
+                }
+                while (!isNumber || number < 0 || number > 9);
+
+                Console.WriteLine($"A beolvasott szám: {number}");
+
             }
+            Console.Clear();
+
             Console.WriteLine($"A teljes végösszeg {all:C2}, fejenként {all / student:C2}");
 
             Console.ReadKey();
