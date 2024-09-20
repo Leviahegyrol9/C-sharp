4c109c407a8ae6f88b4830c26826252fc79aac6d
Paplukács Levente <b2paplev@vasvari.org>
2024-09-20 09:55:31 +0200

    2024.09.20

---
+++ Program/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            List<string> programs = new List<string>();
+            //string[] programs = new string[] {};
+            int student = random.Next(17,28);
+            double all = 0;
+
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"Kérem a program nevét ({i + 1}): ");
+                string program = Console.ReadLine();
+                programs.Add(program);
+            }
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"Kérem a program összegét ({programs[i]}): ");
+                double money = Convert.ToDouble(Console.ReadLine());
+                all += money;
+            }
+            Console.WriteLine($"A teljes végösszeg {all:C2}, fejenként {all / student:C2}");
+
+            Console.ReadKey();
+        }
+    }
+}
