db471f8800ee924cb261309d452772329f1d400d
Paplukács Levente <b2paplev@vasvari.org>
2024-09-25 12:22:24 +0200

    2024.09.25

--- Telefon/Program.cs
+++ Telefon/Program.cs
@@ -11,13 +11,22 @@ namespace Telefon
     {
         static void Main(string[] args)
         {
-            Console.Write("A szoftvert írta: Paplukács Levente, 2024.09.23");
+            string date = DateTime.Now.ToString("yyyy-MM-dd").Replace("-", ".");
+            Console.WriteLine($"A szoftvert írta: Paplukács Levente, {date}");
+
             List<int> samsung = new List<int>();
+            List<int> samsungSize = new List<int>();
             List<int> apple = new List<int>();
+            List<int> appleSize = new List<int>();
             List<int> etc = new List<int>();
+            List<int> etcSize = new List<int>();
+
             double samsungAverage = 0;
             double appleAverage = 0;
             double etcAverage = 0;
+            double samsungAverageSize = 0;
+            double appleAverageSize = 0;
+            double etcAverageSize = 0;
             double sum = 0;
 
             int number = 0;
@@ -52,16 +61,22 @@ namespace Telefon
                 Console.Write("Adja meg a telefon akkumlátor szintjét: ");
                 int battery = Convert.ToInt32(Console.ReadLine());
 
+                Console.Write("Adja meg a telefon kijelző méretét: ");
+                int size = Convert.ToInt32(Console.ReadLine());
+
                 switch (manufacter.ToLower())
                 {
                     case "samsung":
                         samsung.Add(battery);
+                        samsungSize.Add(size);
                         break;
                     case "apple":
                         apple.Add(battery);
+                        appleSize.Add(size);
                         break;
                     default:
                         etc.Add(battery);
+                        etcSize.Add(size);
                         break;
                 }
 
@@ -92,11 +107,43 @@ namespace Telefon
                 etcAverage = sum / etc.Count();
             }
 
+            //---------------------------------------------------------
+            sum = 0;
+
+            foreach (int item in samsungSize)
+            {
+                sum += item;
+
+                samsungAverageSize = sum / samsung.Count();
+            }
+
+            sum = 0;
+
+            foreach (int item in appleSize)
+            {
+                sum += item;
+
+                appleAverageSize = sum / apple.Count();
+            }
+
+            sum = 0;
+
+            foreach (int item in etcSize)
+            {
+                sum += item;
+
+                etcAverageSize = sum / etc.Count();
+            }
+
             Console.Clear();
 
             Console.WriteLine($"A samsung telefonok átlagos kapacitása {samsungAverage:N1}%");
             Console.WriteLine($"Az apple telefonok átlagos kapacitása {appleAverage:N1}%");
-            Console.WriteLine($"Az egyéb telefonok átlagos kapacitása {etcAverage:N1}%");
+            Console.WriteLine($"Az egyéb telefonok átlagos kapacitása {etcAverage:N1}%\n");
+
+            Console.WriteLine($"A samsung telefonok átlagos kijelzőmérete {samsungAverageSize:N1} inch");
+            Console.WriteLine($"Az apple telefonok átlagos kijelzőmérete {appleAverageSize:N1} inch");
+            Console.WriteLine($"Az egyéb telefonok átlagos kijelzőmérete {etcAverageSize:N1} inch");
 
             Console.ReadKey();
         }
