d413624ec5370627203f631b6414fd61a1dc295f
Paplukács Levente <b2paplev@vasvari.org>
2024-10-02 12:31:12 +0200

    2024.10.02

---
+++ Fajlbeolvasas/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fileBeolvasas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //az aktuális könyvtár lekérése
+            string currentDirectory = Directory.GetCurrentDirectory();
+
+            //az összes .txt fájl kilistázása az aktuália mappából
+            string[] txtFiles = Directory.GetFiles(currentDirectory, "*.txt");
+
+            if (txtFiles.Length == 0)
+            {
+                Console.WriteLine("Nincs .txt fájl az aktuális mappában");
+            }
+
+            // Fájlok kilistázása a konzolra
+            Console.WriteLine("Válassz egy fájlt a következők közül: ");
+            for (int i = 0; i < txtFiles.Length; i++)
+            {
+                Console.Write($"{i + 1}. {Path.GetFileName(txtFiles[i])} ");
+            }
+
+            // Felhasználói választás lekérese (ellenőrzés nélkül)
+            Console.Write("Írd be a fájl számát: ");
+            int fileIndex = int.Parse(Console.ReadLine());
+
+            // Kiválasztott fájl neve és elérési útja
+            string selectedFile = txtFiles[fileIndex - 1];
+
+            // A kiválasztot fájl tartalmának beolvasása
+            List<string> fileContent = new List<string>(File.ReadAllLines(selectedFile));
+
+            // Fájl tartalmának megjelenítése soronként
+            Console.WriteLine($"\nA(z) {Path.GetFileName(selectedFile)} tartalma:");
+            foreach (string Line in fileContent)
+            {
+                Console.WriteLine(Line);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
--- SZKT/Program.cs
+++ SZKT/Program.cs
@@ -11,22 +11,39 @@ namespace SZKT
     {
         static void Main(string[] args)
         {
-            List<string> tram1 = new List<string>();
+            List<string> tram1 = new List<string>() { "Szegedi vasútállomás", "Galamb  utca", "Bécsi körút", "Aradi vértanuk tere", "Somogyi utca", "Széchényi tér", "Anna-kút", "Rókusi templom", "Tavasz utca", "Damjanich utca", "Vásárhelyi Pál utca", "Pulz utca", "Rókusi Vasútállomás" };
             int tram1Time = 17;
+
             List<string> tram2 = new List<string>() { "Vértó", "Bálint Sándor utca", "Rókusi víztorony", "Szatymazu utca", "Vásárhelyi Pál út", "Damjanich utca", "Tavasz utca", "Rókusi templom", "Anna-kút", "Széchenyi tér", "Somogyi utca", "Aradi vértanúk tere", "Bécsi krt.", "Bem utca", "Szeged vá." };
             int tram2Time = 18;
-            List<string> tram3 = new List<string>();
-            int tram3Time = 17;
-            List<string> tram3F = new List<string>();
-            int tram3FTime = 17;
+
+            List<string> tram3 = new List<string>() { "Tarján", "Budapesti krt.", "Deák Ferenc Gimnázium", "Rózsa utca", "Kecskeméti utca", "Brüsszeli krt.", "Szent György tér", "Glattfelder Gyula tér", "Anna-kút", "Tisza Lajos krt. (Károlyi u.), ", "Dugonics tér", "Londoni krt. (Kálvária sgt.)", "Veresács u.", "Kálvária tér", "II. Kórház", "vadaspark", "Belvárosi temető", "Belvárosi temető II. kapu", "Kereskedő köz", "Fonógyári út" };
+            int tram3Time = 24;
+
+            List<string> tram3F = new List<string>() { "Tarján", "Budapesti krt.", "Deák Ferenc Gimnázium", "Rózsa utca", "Kecskeméti utca", "Brüsszeli krt.", "Szent György tér", "Glattfelder Gyula tér", "Anna-kút", "Tisza Lajos krt. (Károlyi u.), ", "Dugonics tér", "Londoni krt. (Kálvária sgt.)", "Veresács u.", "Kálvária tér", "II. Kórház", "Vadaspark" };
+            int tram3FTime = 16;
+
             List<string> tram4 = new List<string>() { "Szalámigyár", "Szabadkai út", "Vám tér", "Szivárvány kitérő", "Vitéz utca", "Dugonics tér", "Tisza Lajos krt. (Károlyi u.)", "Anna-kút", "Glattfelder Gyula tér", "Szent György tér", "Brüsszeli krt.", "Kecskeméti utca", "Rózsa utca", "Deák Ferenc Gimnázium", "Budapesti krt.", "Tarján"};
             int tram4Time = 16;
 
-            Console.WriteLine($"A 1-es villamos átlagosan {AverageCalculate(tram1, tram1Time)} perc alatt ér egy megállóhoz.");
-            Console.WriteLine($"A 2-es villamos átlagosan {AverageCalculate(tram2, tram2Time)} perc alatt ér egy megállóhoz.");
-            Console.WriteLine($"A 3-as villamos átlagosan {AverageCalculate(tram3, tram3Time)} perc alatt ér egy megállóhoz.");
-            Console.WriteLine($"A 3F villamos átlagosan {AverageCalculate(tram3F, tram3FTime)} perc alatt ér egy megállóhoz.");
-            Console.WriteLine($"A 4-es villamos átlagosan {AverageCalculate(tram4, tram4Time)} perc alatt ér egy megállóhoz.");
+            List<int> numbers = new List<int>() { tram1Time, tram2Time, tram3Time, tram3FTime, tram4Time };
+
+            Console.WriteLine($"Az 1-es villamos átlagosan {AverageCalculate(tram1, tram1Time):n1} perc alatt ér egy megállóhoz.");
+            Console.WriteLine($"A 2-es villamos átlagosan {AverageCalculate(tram2, tram2Time):n1} perc alatt ér egy megállóhoz.");
+            Console.WriteLine($"A 3-as villamos átlagosan {AverageCalculate(tram3, tram3Time):n1} perc alatt ér egy megállóhoz.");
+            Console.WriteLine($"A 3F-es villamos átlagosan {AverageCalculate(tram3F, tram3FTime):n1} perc alatt ér egy megállóhoz.");
+            Console.WriteLine($"A 4-es villamos átlagosan {AverageCalculate(tram4, tram4Time):n1} perc alatt ér egy megállóhoz.\n");
+
+            Console.WriteLine($"Az 1-es villamos {tram1Time} perc alatt ér a végállomáshoz.");
+            Console.WriteLine($"A 2-es villamos {tram2Time} perc alatt ér a végállomáshoz.");
+            Console.WriteLine($"A 3-as villamos {tram3Time} perc alatt ér a végállomáshoz.");
+            Console.WriteLine($"A 3F-es villamos {tram3FTime} perc alatt ér a végállomáshoz.");
+            Console.WriteLine($"A 4-es villamos {tram4Time} perc alatt ér a végállomáshoz.");
+
+            int minTravelTime = CalulateMin(numbers);
+
+            int maxTravelTime = CalulateMax(numbers);
+
 
             Console.ReadKey();
         }
@@ -35,5 +52,34 @@ namespace SZKT
         {
             return (double) time / tram.Count();
         }
+
+        static int CalulateMin(List<int> numbers)
+        {
+
+            int min = numbers[0];
+            foreach (int number in numbers)
+            {
+                if (number < min)
+                {
+                    min = number;
+                }
+
+            }
+            return min;
+        }
+
+        static int CalulateMax(List<int> numbers)
+        {
+
+            int max = numbers[0];
+            foreach (int number in numbers)
+            {
+                if (number > max)
+                {
+                    max = number;
+                }
+
+            }
+            return max;
+        }
     }
-}
