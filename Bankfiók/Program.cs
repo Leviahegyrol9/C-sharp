65c9d03c90ef8c58f1d61930c4e1639bbe3fd2c2
Paplukács Levente <b2paplev@vasvari.org>
2024-10-10 14:25:08 +0200

    2024.10.10

--- Bankfiók/Program.cs
+++ Bankfiók/Program.cs
@@ -12,7 +12,11 @@ namespace Bankfiók
         {
             string[] names = new string[10] { "Ügyfél1", "Ügyfél2", "Ügyfél3", "Ügyfél4", "Ügyfél5", "Ügyfél6", "Ügyfél7", "Ügyfél8", "Ügyfél9", "Ügyfél10" };
 
-            int[,] namesAndValues = new int[10, 12];
+            List<int> averages = new List<int> ();
+            List<int> sums = new List<int> ();
+            int sum = 0;
+
+            int[,] values = new int[10, 12];
 
             int counter = 0;
 
@@ -22,14 +26,33 @@ namespace Bankfiók
 
                 for (int j = 0; j < 12; j++)
                 {
-                    namesAndValues[counter, j] = rnd.Next(-500000, 1000000);
+                    values[counter, j] = rnd.Next(-500000, 1000000);
 
                 }
                 counter++;
 
             }
 
-            for (int i = 0; namesAndValues)
+            counter = 0;
+
+            for (int i = 0; i < names.Count(); i++)
+            {
+
+                Console.Write($"\n{names[i]}\t\t");
+
+                for (int j = 0; j < 12; j++)
+                {
+                    Console.Write($"{values[counter, j]}\t");
+                    summs.Add();
+                }
+                counter++;
+                Console.WriteLine();
+                
+
+            }
+
+
+            Console.ReadKey();
         }
     }
 }
