cf4db25247f873cd5bc3a875dd8d4433c92a39e4
Paplukács Levente <b2paplev@vasvari.org>
2024-10-21 10:40:59 +0200

    2024.10.21

---
+++ Viragfold/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Viragfold
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+
+            List<string> names = new List<string>();
+            List<int> flowerbed = new List<int>();
+            int counter = 0;
+
+            for (int i = 1; i < 61; i++)
+            {
+                if(i <= 30)
+                {
+                    names.Add("A-" + i);
+                }
+                else
+                {
+                    counter++;
+                    names.Add("B-" + counter);
+
+                }
+            }
+            for (int i = 0; i < names.Count(); i++) 
+            {
+                flowerbed.Add(rnd.Next(0,201));
+            }
+
+            for (int i = 0; i < names.Count(); i++)
+            {
+                Console.WriteLine(names[i] + " " + flowerbed[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
