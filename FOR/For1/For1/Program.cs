993d0bd5f17ae356ef6436a738133dd9531cd8cb
Leviahegyrol9 <paplukacs.levi@gmail.com>
2024-11-10 15:24:44 +0100

    2024.11.10

--- FOR/For1/For1/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿Console.WriteLine("Számok 1-től 10-ig");
-int i;
-for(i=1;1<=10;i++)
-{
-    Console.WriteLine(i);
-}
--- FOR/For2/For2/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿int i;
-int osszeg = 0;
-Console.WriteLine("Kérek 10 számot!");
-for(i=1;i<=10;i++)
-{
-    int szam = Convert.ToInt32(Console.ReadLine());
-    osszeg = osszeg + szam;
-}
-Console.WriteLine("A számok összeg:" + osszeg);
