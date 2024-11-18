a06e3de2a926c643c4395e7c41501d2fb4388552
Leviahegyrol9 <paplukacs.levi@gmail.com>
2024-11-18 20:43:04 +0100

    2024.11.18

--- Osztalyzatok/Program.cs
+++ Osztalyzatok/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom.Compiler;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
@@ -11,42 +12,125 @@ namespace Osztalyzatok
 {
     internal class Program
     {
-        static List<string> textToList(string filePath)
+        static void PrintSubjectAverage(List<string> subjects)
         {
-            // A fájl beolvasása
-            string[] lines = File.ReadAllLines(filePath);  // Minden sor beolvasása
+            // Tantárgyi átlag
+            Dictionary<string, List<double>> subjectsAndMarks = new Dictionary<string, List<double>>();
+            string[] lines = File.ReadAllLines("osztalyzatok.txt");
 
-            // Tömbök a nevekre és a befizetett összegekre
-
-            List<string> datas = new List<string>(lines.Length);
-
-            // A fájl sorainak feldolgozása
-            for (int i = 0; i < lines.Length; i++)
+            foreach (string subject in subjects)
             {
-                datas.Add(lines[i]);
+                subjectsAndMarks[subject] = new List<double>();
             }
 
+            foreach (string line in lines)
+            {
+                string[] oneLine = line.Split(';');
+
+                for (int i = 1; i < oneLine.Length; i++)
+                {
+                    double mark = double.Parse(oneLine[i]);
+
+                    if (mark != 0)
+                    {
+                        subjectsAndMarks[subjects[i - 1]].Add(mark);
+                    }
+                }
+            }
+
+            // Tantárgyi átlagok kiírása
+            Console.WriteLine("\nTantárgyak átlagai:\n");
+
+            foreach (KeyValuePair<string, List<double>> data in subjectsAndMarks)
+            {
+                Console.WriteLine($"{data.Key}: {data.Value.Sum() / data.Value.Count():N2}");
+            }
+        }
+        static void PrintStudentAverage()
+        {
+            // Diákátlag
+            Dictionary<string, List<double>> studentsAndMarks = new Dictionary<string, List<double>>();
+            string[] lines = File.ReadAllLines("osztalyzatok.txt");
+
+            foreach (string line in lines)
+            {
+                string[] oneLine = line.Split(';');
+                string student = oneLine[0];
+
+                List<double> tempMarks = new List<double>();
+
+                for (int i = 1; i < oneLine.Length; i++)
+                { 
+                    if (double.Parse(oneLine[i]) != 0)
+                    {
+                        tempMarks.Add(double.Parse(oneLine[i]));
+                    }
+                }
+
+                studentsAndMarks.Add(student, tempMarks);
+            }
+
+            // Átlagok kiírása
+            foreach (KeyValuePair<string, List<double>> data in studentsAndMarks)
+            {
+                Console.WriteLine($"{data.Key}: {data.Value.Sum() / data.Value.Count():N2}");
+            }
+        }
+        static List<string> ReadFile(string fileName)
+        {
+            string[] lines = File.ReadAllLines(fileName);
+
+            List<string> datas = new List<string>();
+
+            foreach (string line in lines)
+            {
+                datas.Add(line);
+            }
+           
             return datas;
+        }
+
+        static bool PrintToFile(List<string> names, List<string> subjects, string fileName)
+        {
+            Random random = new Random();
+
+            try
+            {
+                using (StreamWriter writer = new StreamWriter(fileName))
+                {
+                    for (int i = 0; i < names.Count; i++)
+                    {
+                        writer.Write($"{names[i]}");
+
+                        for (int j = 0; j < subjects.Count; j++)
+                        {
+                            int mark = random.Next(0, 6);
+                            writer.Write($";{mark}");
+                        }
+                        writer.WriteLine();
+                    }
+                }
+                return true;
+            }
+            catch (Exception)
+            {
+                return false;
+            }
         }
 
         static void Main(string[] args)
         {
             int option = 0;
             bool isNumber = false;
-            Random random = new Random();
-            Dictionary<string, List<int>> namesAndMarks = new Dictionary<string, List<int>>();
 
-            string filePath = "tantargyak.txt";
-            List<string> subjects = textToList(filePath);
+            List<string> subjects = ReadFile("tantargyak.txt");
 
-            filePath = "magyar_nevek.txt";
-            List<string> names = textToList(filePath);
-
+            List<string> names = ReadFile("magyar_nevek.txt");
 
             while (!isNumber || (option > 2 || option < 1))
             {
                 Console.WriteLine("1. Létrehozás: Adatok generálása és fájlba mentése.\n2. Elemzés: Az osztalyzatok.txt és a tantárgyakat tartalmazó fájl alapján elemzés végrehajtása.");
-                Console.Write("Írja be a választott opciót: ");
+                Console.Write("\nÍrja be a választott opciót: ");
                 string input = Console.ReadLine();
 
                 isNumber = int.TryParse(input, out option);
@@ -67,45 +151,31 @@ namespace Osztalyzatok
 
             if (option == 1)
             {
-                using (StreamWriter writer = new StreamWriter("osztalyzatok.txt"))
-                {
-                    for (int i = 0; i < names.Count(); i++)
-                    {
-                        writer.Write($"{names[i]}");
+                Console.Clear();
 
-                        for (int j = 0; j < subjects.Count(); j++)
-                        {
-                            int mark = random.Next(0, 6);
-                            writer.Write($";{mark}");
-                        }
-                        writer.WriteLine();
-                    }
+                string fileName = "osztalyzatok.txt";
+                bool success = PrintToFile(names, subjects, fileName);
+
+                if (success)
+                {
+                    Console.Write($"Az {fileName} állomány sikeresen létrehozva!");
+                }
+                else
+                {
+                    Console.Write($"Az {fileName} állományt nem sikerült létrehozni!");
                 }
             }
             else if (option == 2)
             {
-                List<int> tempMarks = new List<int>();
-                // A fájl beolvasása
-                string[] lines = File.ReadAllLines("osztalyzatok.txt");  // Minden sor beolvasása
+                Console.Clear();
+                Console.WriteLine("Tanulók átlagai:\n");
+                PrintStudentAverage();
 
-                for (int i = 0; i < lines.Length; i++)
-                {
-                    string[] parts = lines[i].Split(';');
-                    string name = parts[0].Trim();
-                    for (int j = 1; j < parts.Length; j++)
-                    {
-                        tempMarks.Add(int.Parse(parts[j].Trim()));
-                    }
-                    namesAndMarks.Add(name, tempMarks);
-                }
-
-                foreach (KeyValuePair<string, List<int>> data in namesAndMarks)
-                {
-                    Console.WriteLine($"{data.Key}, jegyek átlaga: {data.Value.Sum()/subjects.Count()}");
-                }
+                PrintSubjectAverage(subjects);
             }
 
             Console.ReadKey();
+
         }
     }
 }
