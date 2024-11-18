07c6d0ee3e29bb4c22a6df166bf6f3042a5a3fb2
Leviahegyrol69 <paplukacs.levi@gmail.com>
2024-11-18 15:13:33 +0100

    2024.11.18

--- Osztalyzatok/Program.cs
+++ Osztalyzatok/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -17,21 +18,29 @@ namespace Osztalyzatok
 
             // Tömbök a nevekre és a befizetett összegekre
 
-            List<string> names = new List<string>(lines.Length);
+            List<string> datas = new List<string>(lines.Length);
 
             // A fájl sorainak feldolgozása
             for (int i = 0; i < lines.Length; i++)
             {
-                names.Add(lines[i]);  // Sor felbontása névre és összegre
+                datas.Add(lines[i]);
             }
 
-            return names;
+            return datas;
         }
 
         static void Main(string[] args)
         {
             int option = 0;
             bool isNumber = false;
+            Random random = new Random();
+            Dictionary<string, List<int>> namesAndMarks = new Dictionary<string, List<int>>();
+
+            string filePath = "tantargyak.txt";
+            List<string> subjects = textToList(filePath);
+
+            filePath = "magyar_nevek.txt";
+            List<string> names = textToList(filePath);
 
 
             while (!isNumber || (option > 2 || option < 1))
@@ -55,9 +64,46 @@ namespace Osztalyzatok
                     Console.Clear();
                 }
             }
-            string filePath = "tantargyak.txt";
 
-            List<string> subjects = textToList(filePath);
+            if (option == 1)
+            {
+                using (StreamWriter writer = new StreamWriter("osztalyzatok.txt"))
+                {
+                    for (int i = 0; i < names.Count(); i++)
+                    {
+                        writer.Write($"{names[i]}");
+
+                        for (int j = 0; j < subjects.Count(); j++)
+                        {
+                            int mark = random.Next(0, 6);
+                            writer.Write($";{mark}");
+                        }
+                        writer.WriteLine();
+                    }
+                }
+            }
+            else if (option == 2)
+            {
+                List<int> tempMarks = new List<int>();
+                // A fájl beolvasása
+                string[] lines = File.ReadAllLines("osztalyzatok.txt");  // Minden sor beolvasása
+
+                for (int i = 0; i < lines.Length; i++)
+                {
+                    string[] parts = lines[i].Split(';');
+                    string name = parts[0].Trim();
+                    for (int j = 1; j < parts.Length; j++)
+                    {
+                        tempMarks.Add(int.Parse(parts[j].Trim()));
+                    }
+                    namesAndMarks.Add(name, tempMarks);
+                }
+
+                foreach (KeyValuePair<string, List<int>> data in namesAndMarks)
+                {
+                    Console.WriteLine($"{data.Key}, jegyek átlaga: {data.Value.Sum()/subjects.Count()}");
+                }
+            }
 
             Console.ReadKey();
         }
