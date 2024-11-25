610b3ecff5630f12a48c2489b0a61fe6786afaf8
Leviahegyrol69 <paplukacs.levi@gmail.com>
2024-11-25 15:00:52 +0100

    2024.11.25

---
+++ Kőpapírolló/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Kőpapírolló
+{
+    internal class Program
+    {
+        static string Capitalize(string name)
+        {
+            string[] names = name.Split(' ');
+
+            if (names.Length == 1)
+            {
+                names[0] = char.ToUpper(names[0][0]) + names[0].Substring(1);
+                name = names[0];
+            }
+            else if (names.Length == 2)
+            {
+                names[0] = char.ToUpper(names[0][0]) + names[0].Substring(1);
+                names[1] = char.ToUpper(names[1][0]) + names[1].Substring(1);
+                name = $"{names[0]} {names[1]}";
+            }
+            else if (names.Length == 3)
+            {
+                names[0] = char.ToUpper(names[0][0]) + names[0].Substring(1);
+                names[1] = char.ToUpper(names[1][0]) + names[1].Substring(1);
+                names[2] = char.ToUpper(names[2][0]) + names[2].Substring(1);
+                name = $"{names[0]} {names[1]} {names[2]}";
+            }
+
+            return name;
+        }
+        static int GetOption(string prompt)
+        {
+            bool isNumber;
+            int option;
+            do
+            {
+                Console.Clear();
+
+                Console.WriteLine(prompt);
+                Console.Write("\nOpció: ");
+                string input = Console.ReadLine();
+
+                isNumber = int.TryParse(input, out option);
+
+                if (!isNumber)
+                {
+                    Console.Write("Nem számot adott meg!");
+                    Thread.Sleep(2500);
+                }
+                else if (option < 1 || option > 3)
+                {
+                    Console.Write("Nincs ilyen opció!");
+                    Thread.Sleep(2500);
+                }
+
+            } while (!isNumber || option < 1 || option > 3);
+
+            return option;
+        }
+        static string GetName(string prompt)
+        {
+            bool isString;
+            string name;
+
+            do
+            {
+                Console.Clear();
+
+                Console.Write(prompt);
+                name = Console.ReadLine().Trim();
+
+                isString = name.Replace(" ", "").All(char.IsLetter);
+
+                if (!isString || name.Length < 3)
+                {
+                    Console.Write("Nem nevet adott meg!");
+                    Thread.Sleep(2500);
+                }
+
+            } while (!isString || name.Length < 3);
+
+            return Capitalize(name);
+        }
+        static void Main(string[] args)
+        {
+            int counter = 0;
+
+            List<int> option1 = new List<int>();
+            List<int> option2 = new List<int>();
+
+            string name1 = GetName("Kérem az első nevet: ");
+
+            string name2 = GetName("Kérem a második nevet: ");
+
+            while (counter < 10)
+            {
+                counter++;
+
+                option1.Add(GetOption($"{name1} következik:\n1 - Kő\n2 - Papír\n3 - Olló"));
+
+                option2.Add(GetOption($"{name2} következik:\n1 - Kő\n2 - Papír\n3 - Olló"));
+
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ SQL/Megkötések mintapélda.sql
@@ -0,0 +1,92 @@
+﻿CREATE DATABASE iskola CHARACTER SET utf8 COLLATE utf8_hungarian_ci;
+USE iskola;
+
+CREATE TABLE diak(
+    diakAZ int PRIMARY KEY,
+    nev varchar(100) NOT null,
+    email varchar(100) NOT null UNIQUE,
+    szuldatum date NOT null,
+    testverszam tinyint,
+    INDEX(nev),
+    INDEX szuldatum_idx(szuldatum)
+);
+
+-- A Not null azt jelenti, hogy a mező nem lehet üres.
+-- A UNIQUE azt jelenti, hogy a mező egyedi értékeket tartalmazhat, nem lehet két egyforma érték benne.
+-- Az index megadása kétféleképpen történt. Az első esetben az indexnév a mezőnévvel megegyezik, a második esetben mi adtunk indexnevet (szuldatum_idx).
+
+DROP INDEX szuldatum_idx ON diak;
+-- index törlésének egyik módja
+
+CREATE UNIQUE INDEX email_idx ON diak(email);
+-- egyedi index létrehozásának egyik módja
+-- mivel már létezett az email mezőre egy index, jelzi, hogy ez duplikált index, de végrehajtja
+
+ALTER TABLE diak DROP INDEX email;
+-- index törlésének másik módja - érdemes törölni, hogy egyféle index legyen
+
+ALTER TABLE diak DROP diakAZ;
+-- mező törlése
+
+ALTER TABLE diak ADD diakID int AUTO_INCREMENT PRIMARY KEY FIRST;
+-- új elsődleges kulcs mező létrehozása automatikus sorszámként
+
+INSERT INTO `diak`(`nev`, `email`, `szuldatum`, `testverszam`) VALUES 
+('Varga Zsolt','varga.zsolt@vasvari.org','2007-05-26',0),
+('Hajós Álmos','almoshajos@vasvari.org','2007-02-10',3),
+('Tolnai Renáta','tolnairencsi@gmail.com','2006-12-11',1);
+-- tábla feltöltése
+
+INSERT INTO `diak`(`nev`, `email`, `szuldatum`, `testverszam`) VALUES 
+('Karika Árpád','varga.zsolt@vasvari.org','2008-04-13',2);
+-- ez a parancs nem fut le, mert az email cím mezőben duplikált érték van
+
+INSERT INTO `diak`(`nev`, `email`, `szuldatum`, `testverszam`) VALUES 
+('Karika Árpád','karikaarpika@vasvari.org','2008-04-13',2);
+-- javított rekordbeszúrás
+
+ALTER TABLE diak ADD osztaly varchar(6);
+-- a diak táblához új mező hozzáadása
+
+UPDATE diak SET osztaly = "9.a";
+-- az új mező feltöltése azonos értékekkel
+
+CREATE TABLE osztalyok(
+	osztalyAZ varchar(6) PRIMARY KEY,
+    osztalyfonok varchar(40) NOT null,
+    osztalyterem tinyint UNIQUE
+);
+-- Az osztalyok tábla létrehozása, amelyet a diak táblához fogunk majd kapcsolni
+
+INSERT INTO `osztalyok`(`osztalyAZ`, `osztalyfonok`, `osztalyterem`) VALUES 
+('9.a','Vass Mária',30),
+('9.b',null,14),
+('9.c','Kovácsné Kertész Etelka',32)
+
+-- Az osztalyok tábla feltöltése - mivel a név mező nem lehett null, azért figyelmeztetést kapunk, de a parancs lefut.
+-- Üzenet: "Warning: #1048 A(z) 'osztalyfonok' oszlop erteke nem lehet nulla"
+
+ALTER TABLE diak ADD FOREIGN KEY (osztaly) REFERENCES osztalyok(osztalyAZ);
+-- Idegen kulcs létrehozása a diak tábla osztaly mezőjére
+
+-- A kapcsolathoz alapvetően a szigorú szabályrendszer kapcsolódik (RESTRICT)
+-- Ebben az esetben az elsődleges kulcsot nem lehet törölni vagy módosítani, ha van kapcsolódó rekord a másik táblában, ill. nem lehet olyan tanulót felvenni a diak táblába, akinek nem létezik az osztálya a másik táblában.
+
+DELETE FROM osztalyok WHERE osztalyAZ = "9.a";
+UPDATE osztalyok SET osztalyAZ = "9.m" WHERE osztalyAZ = "9.a";
+-- Egyik parancs sem fut le, mert a 9.a osztályban vannak diákok
+-- Hibaüzenet: "Cannot delete or update a parent row: a foreign key constraint fails (`iskola`.`diak`, CONSTRAINT `diak_ibfk_1` FOREIGN KEY (`osztaly`) REFERENCES `osztalyok` (`osztalyAZ`))"
+
+INSERT INTO `diak`(`nev`, `email`, `szuldatum`, `testverszam`, `osztaly`) VALUES
+('Hajas Árpád','arpadhaj@gmail.com','2007-03-10',2,'10.a');
+-- Ez a parancs sem fut le, mert az osztalyok táblában nincs felvéve 10.a osztály, nem tudja mihez kapcsolni a rekordot.
+-- Hibaüzenet: "Cannot add or update a child row: a foreign key constraint fails (`iskola`.`diak`, CONSTRAINT `diak_ibfk_1` FOREIGN KEY (`osztaly`) REFERENCES `osztalyok` (`osztalyAZ`))"
+
+-- Ha a kapcsolatnál az ON DELETE / ON UPDATE CASCADE-ra van átállítva, akkor lehetséges a fenti törlés és módosítás
+
+UPDATE osztalyok SET osztalyAZ = "9.m" WHERE osztalyAZ = "9.a";
+-- A 9.a osztály mostantól 9.m lesz és a diak táblában minden 9.a-diáknál a 9.m osztály fog látszódni.
+
+DELETE FROM osztalyok WHERE osztalyAZ = "9.m";
+-- A 9.m osztályt kitörli és ezzel együtt az összes olyan diákot is a diak táblából, aki ebbe az osztályba jár.
+
