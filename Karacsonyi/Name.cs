329662b2f1a3924a8191c32fd9ad4990456edc72
Leviahegyrol69 <paplukacs.levi@gmail.com>
2024-12-11 12:29:53 +0100

    2024.12.11

---
+++ Karacsonyi/Name.cs
@@ -0,0 +1,26 @@
+﻿namespace Model
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public string Present1 { get; set; }
+        public string Present2 { get; set; }
+
+        public Person()
+        {
+        }
+
+        public Person(string name, string present1, string present2)
+        {
+            Name = name;
+            Present1 = present1;
+            Present2 = present2;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}: {Present1}, {Present2}";
+        }
+    }
+}
+
---
+++ Karacsonyi/PersonIO.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Model;
+
+namespace IO
+{
+    public static class PersonIO
+    {
+        public static List<Model.Person> ReadFile(string path)
+        {
+            List<Model.Person> diakok = new List<Model.Person>();
+
+            string[] lines = File.ReadAllLines(path);
+
+            foreach (string line in lines)
+            {
+                string[] datas = line.Split('\t');
+
+                Model.Person diak = new Model.Person
+                {
+                    Name = datas[0],
+                    Present1 = datas[1],
+                    Present2 = datas[2]
+                };
+
+                diakok.Add(diak);
+            }
+
+            return diakok;
+        }
+
+    }
+}
