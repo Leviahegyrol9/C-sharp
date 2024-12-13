6fc2987a3b17cf736571e66c7af11b9a5d63fd58
Leviahegyrol9 <paplukacs.levi@gmail.com>
2024-12-13 23:27:20 +0100

    2024.12.13

---
+++ Beolvasas2/Klub.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Beolvasas2
+{
+    public class Klub
+    {
+        public string Name { get; set; }
+        public int Number { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public bool HunNationality { get; set; }
+        public bool AbrNationality { get; set; }
+        public int Value { get; set; }
+        public string Post { get; set; }
+
+        public Klub()
+        {
+
+        }
+
+        public Klub(string name, int number, string firstName, string lastName, DateTime birthDate, bool hunNationality, bool abrNationality, int value, string post)
+        {
+            Name = name;
+            Number = number;
+            FirstName = firstName;
+            LastName = lastName;
+            BirthDate = birthDate;
+            HunNationality = hunNationality;
+            AbrNationality = abrNationality;
+            Value = value;
+            Post = post;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, {Number}, {FirstName}, {LastName}, {BirthDate.ToString("yyyy.MM.dd.")}, {HunNationality}, {AbrNationality}, {Value}, {Post}";
+        }
+    }
+}
---
+++ Beolvasas2/Program.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beolvasas2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string fileName = "adatok.txt";
+            List<Klub> klubs = KlubIO.ReadFile(fileName);
+
+            //A kapusokon kívül mindenkit mezőnyjátékosnak tekintünk. Keresse ki a legidősebb mezőnyjátékos vezeték- és utónevét, valamint születési dátumát! (Feltételezheti, hogy csak egy ilyen játékos van.)
+
+            Klub oldestPlayer = klubs.Single(klub => klub.BirthDate == klubs.Min(min => min.BirthDate));
+
+            Console.WriteLine($"A legidősebb játékos: {oldestPlayer.LastName} {oldestPlayer.FirstName} ({oldestPlayer.BirthDate.ToString("yyyy.MM.dd.")})");
+
+            //Határozza meg hány magyar, külföldi és kettős állampolgárságú játékos van!
+
+            int hunNationality = klubs.Count(klub => klub.HunNationality == true); 
+            int abrNationality = klubs.Count(klub => klub.AbrNationality == true);
+
+            Console.WriteLine();
+
+            Console.WriteLine($"Magyar nemzetiségű játékosok száma: {hunNationality}\nKülföldi nemzetiségű játékosok száma: {abrNationality}");
+
+            //Határozza meg játékosok összértékét csapatonként és írja ki a képernyőre! A csapatok neve és a játékosainak összértéke jelenjen meg!
+
+            Dictionary<string, int> teamsAndValues = GetTeamsAndValues(klubs);
+
+            Console.WriteLine();
+
+            foreach (KeyValuePair<string, int> team in teamsAndValues)
+            {
+                Console.WriteLine($"{team.Key}: {team.Value} EUR");
+            }
+
+            Console.WriteLine();
+
+            //Keressük ki azon játékosokat, akiknek az értékük nem haladja meg a játékosok értékének átlag értékét.
+
+            List<Klub> underAveragePlayers = klubs.Where(klub => klub.Value < klubs.Average(avg => avg.Value)).ToList();
+
+            foreach (Klub player in underAveragePlayers)
+            {
+                Console.WriteLine($"{player.LastName} {player.FirstName} ({player.Value} EUR)");
+            }
+
+            Console.WriteLine();
+
+            //Írja ki azon játékosok nevét, születési dátumát és csapataik nevét, akik 18 és 21 év közt vannak és magyar állampolgárok. Ha nincs ilyen, akkor megfelelő üzenettel helyettesítse a kimenetet.
+
+            DateTime today = DateTime.Now;
+
+            List<Klub> betweenPlayers = klubs.Where(klub => klub.HunNationality == true && (klub.BirthDate.Year - today.Year >= 18 && klub.BirthDate.Year - today.Year <= 21)).ToList();
+
+            if (betweenPlayers.Count != 0)
+            {
+                foreach (Klub player in betweenPlayers)
+                {
+                    Console.WriteLine($"{player.Name} {player.LastName} {player.FirstName} ({player.BirthDate.ToString("yyyy.MM.dd.")})");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Nincs ilyen játékos!");
+            }
+
+            Console.ReadKey();
+        }
+
+        private static Dictionary<string, int> GetTeamsAndValues(List<Klub> klubs)
+        {
+            Dictionary<string, int> teamsAndValues = new Dictionary<string, int>();
+
+            HashSet<string> teams = GetAllTeams(klubs);
+
+            foreach (string team in teams)
+            {
+                teamsAndValues[team] = 0;
+            }
+
+            foreach (var klub in klubs)
+            {
+                teamsAndValues[klub.Name] += klub.Value;
+            }
+
+            return teamsAndValues;
+        }
+
+        private static HashSet<string> GetAllTeams(List<Klub> klubs)
+        {
+            HashSet<string> teams = new HashSet<string>();
+
+            foreach (Klub klub in klubs)
+            {
+                teams.Add(klub.Name);
+            }
+
+            return teams;
+        }
+    }
+}
