88019a0b65fb23d15a84d76b57c6468eea764761
Leviahegyrol9 <paplukacs.levi@gmail.com>
2024-12-13 23:38:10 +0100

    2024.12.13

--- Beolvasas2/KlubIO.cs
+++ Beolvasas2/KlubIO.cs
@@ -17,44 +17,52 @@ namespace Beolvasas2
 
             Klub klub = new Klub();
 
-            string[] lines = File.ReadAllLines(fileName);
-
-            foreach (string line in lines)
+            try
             {
-                string[] datas = line.Split('\t');
-                
-                if (datas.Length == 9)
-                {
-                    klub = new Klub
-                    {
-                        Name = datas[0],
-                        Number = int.Parse(datas[1]),
-                        FirstName = datas[2],
-                        LastName = datas[3],
-                        BirthDate = DateTime.Parse(datas[4]),
-                        HunNationality = int.Parse(datas[5]) == -1 ? true : false,
-                        AbrNationality = int.Parse(datas[6]) == -1 ? true : false,
-                        Value = int.Parse(datas[7]),
-                        Post = datas[8]
-                    };
-                }
-                else
-                {
-                    klub = new Klub
-                    {
-                        Name = datas[0],
-                        Number = int.Parse(datas[1]),
-                        FirstName = datas[2],
-                        BirthDate = DateTime.Parse(datas[3]),
-                        HunNationality = int.Parse(datas[4]) == -1 ? true : false,
-                        AbrNationality = int.Parse(datas[5]) == -1 ? true : false,
-                        Value = int.Parse(datas[6]),
-                        Post = datas[7]
-                    };
-                }
-                
+                string[] lines = File.ReadAllLines(fileName);
 
-                klubs.Add(klub);
+                foreach (string line in lines)
+                {
+                    string[] datas = line.Split('\t');
+
+                    if (datas.Length == 9)
+                    {
+                        klub = new Klub
+                        {
+                            Name = datas[0],
+                            Number = int.Parse(datas[1]),
+                            FirstName = datas[2],
+                            LastName = datas[3],
+                            BirthDate = DateTime.Parse(datas[4]),
+                            HunNationality = int.Parse(datas[5]) == -1 ? true : false,
+                            AbrNationality = int.Parse(datas[6]) == -1 ? true : false,
+                            Value = int.Parse(datas[7]),
+                            Post = datas[8]
+                        };
+                    }
+                    else
+                    {
+                        klub = new Klub
+                        {
+                            Name = datas[0],
+                            Number = int.Parse(datas[1]),
+                            FirstName = datas[2],
+                            BirthDate = DateTime.Parse(datas[3]),
+                            HunNationality = int.Parse(datas[4]) == -1 ? true : false,
+                            AbrNationality = int.Parse(datas[5]) == -1 ? true : false,
+                            Value = int.Parse(datas[6]),
+                            Post = datas[7]
+                        };
+                    }
+
+                    klubs.Add(klub);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+                Environment.Exit(0);
             }
 
             return klubs;
