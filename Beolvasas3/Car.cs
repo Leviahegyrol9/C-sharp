df84059b4dafc4a15e865e06f324719249ef2a56
Leviahegyrol9 <paplukacs.levi@gmail.com>
2024-12-19 16:46:12 +0100

    2024.12.19

---
+++ Beolvasas3/Car.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beolvasas3
+{
+    public class Car
+    {
+        public string Name { get; set; }
+        public double MPG { get; set; }
+        public int Cylinder { get; set; }
+        public double Newton { get; set; }
+        public double HorsePower { get; set; }
+        public int Weight { get; set; }
+        public double Acceleration { get; set; }
+        public int Year { get; set; }
+        public string Manufacter { get; set; }
+
+        public Car()
+        {
+            
+        }
+
+        public Car(string name, double mPG, int cylinder, double newton, double horsePower, int weight, double acceleration, int year, string source)
+        {
+            Name = name;
+            MPG = mPG;
+            Cylinder = cylinder;
+            Newton = newton;
+            HorsePower = horsePower;
+            Weight = weight;
+            Acceleration = acceleration;
+            Year = year;
+            Manufacter = source;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name}, {MPG}, {Cylinder}, {Newton}, {HorsePower}, {Weight}, {Acceleration}, {Year}, {Manufacter}";
+        }
+    }
+}
--- Beolvasas3/Program.cs
+++ Beolvasas3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,146 @@ namespace Beolvasas3
     {
         static void Main(string[] args)
         {
+            const string fileName = "autok.csv";
+
+            List<Car> cars = CarIO.ReadCarsFromFile(fileName);
+
+            //1
+
+            Console.WriteLine($"A {fileName} állományban {cars.Count} autó szerepel.\n");
+
+            //2
+
+            Console.WriteLine($"A {fileName} állományban szerepelő autók ösz súlya {cars.Sum(car => car.Weight)}kg.\n");
+
+            //3
+
+            Car HighestNewton = cars.Where(car => car.Newton == cars.Max(max => max.Newton)).Single();
+
+            Console.WriteLine($"A legnagyobb nyomatékkal a {HighestNewton.Name} rendelkezik ({HighestNewton.Newton}Nm).\n");
+
+            //4
+
+            bool AnyCarUnder7s = cars.Any(car => car.Acceleration < 7);
+
+            Console.WriteLine($"{(AnyCarUnder7s ? "Van" : "Nincs")} olyan autó, amely a 7s alatt gyorsul fel 100km/h sebességre.\n");
+
+            //5
+
+            Dictionary<string, int> manufacterAndCount = GetManufacterAndCount(cars);
+
+            PrintManufacterAndCount(manufacterAndCount);
+
+            //6
+
+            const string newFileName = "evek.txt";
+
+            bool success = WriteYearAndNames(cars, newFileName);
+
+            Console.WriteLine($"\nA mentés {(success ? "sikeres volt." : "nem volt sikeres.")}");
+
+            Console.ReadKey();
+        }
+
+        private static Dictionary<string, int> GetManufacterAndCount(List<Car> cars)
+        {
+            Dictionary<string, int> manufacterAndCount = new Dictionary<string, int>();
+
+            HashSet<string> manufacters = GetManufacters(cars);
+
+            foreach (string manufacter in manufacters)
+            {
+                manufacterAndCount[manufacter] = 0;
+            }
+
+            foreach (Car car in cars)
+            {
+                manufacterAndCount[car.Manufacter]++;
+            }
+
+            return manufacterAndCount;
+        }
+
+        private static HashSet<string> GetManufacters(List<Car> cars)
+        {
+            HashSet<string> manufacters = new HashSet<string>();
+
+            foreach (Car car in cars)
+            {
+                manufacters.Add(car.Manufacter);
+            }
+
+            return manufacters;
+        }
+
+        private static void PrintManufacterAndCount(Dictionary<string, int> manufacterAndCount)
+        {
+            foreach (KeyValuePair<string, int> item in manufacterAndCount)
+            {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
+        }
+
+        private static bool WriteYearAndNames(List<Car> cars, string fileName)
+        {
+            Dictionary<int, List<string>> yearsAndNames = GetYearsAndNames(cars);
+
+            StreamWriter writer = new StreamWriter(fileName);
+
+            try
+            {
+                foreach (KeyValuePair<int, List<string>> item in yearsAndNames)
+                {
+                    writer.WriteLine($"{item.Key}:");
+
+                    foreach(string name in item.Value)
+                    {
+                        writer.WriteLine($"\t- {name}");
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                return false;
+            }
+            finally
+            {
+                writer.Close();
+            }
+
+            return true;
+        }
+
+        private static Dictionary<int, List<string>> GetYearsAndNames(List<Car> cars)
+        {
+            Dictionary<int, List<string>> yearsAndNames = new Dictionary<int, List<string>>();
+
+            HashSet<int> years = GetYears(cars);
+
+            foreach (int year in years)
+            {
+                yearsAndNames[year] = new List<string>();
+            }
+
+            foreach (Car car in cars)
+            {
+                yearsAndNames[car.Year].Add(car.Name);
+            }
+
+            Dictionary<int, List<string>> sortedYearsAndNames = yearsAndNames.OrderBy(year => year.Key).ToDictionary(year => year.Key, year => year.Value);
+
+            return sortedYearsAndNames;
+        }
+        private static HashSet<int> GetYears(List<Car> cars)
+        {
+            HashSet<int> years = new HashSet<int>();
+
+            foreach (Car car in cars)
+            {
+                years.Add(car.Year);
+            }
+
+            return years;
         }
     }
 }
