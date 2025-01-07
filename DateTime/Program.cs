a85dc9945d4a61cbe2c433faedcbc351a9e34212
Leviahegyrol9 <paplukacs.levi@gmail.com>
2025-01-07 21:43:56 +0100

    2025.01.07 - 21:43

---
+++ DateTime/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string time1 = "20:50:00";
+            string time2 = "21:30:00";
+
+            // Konvertáljuk az időket DateTime formátumba
+            DateTime date1 = DateTime.ParseExact(time1, "HH:mm:ss", null);
+            DateTime date2 = DateTime.ParseExact(time2, "HH:mm:ss", null);
+
+            // Időeltérés kiszámítása
+            TimeSpan difference = date2 - date1;
+
+            // Az eltelt órák és percek
+            Console.WriteLine($"Eltelt idő: {difference.Hours} óra és {difference.Minutes} perc.");
+            Console.ReadKey();
+        }
+    }
+}
