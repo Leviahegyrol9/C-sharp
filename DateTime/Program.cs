aecab8e4f39ab51b9b4a811a61a822e02e641045
Leviahegyrol9 <paplukacs.levi@gmail.com>
2025-01-10 20:43:13 +0100

    2025.01.10 - 20:43

--- DateTime/Program.cs
+++ DateTime/Program.cs
@@ -13,14 +13,11 @@ namespace ConsoleApp1
             string time1 = "20:50:00";
             string time2 = "21:30:00";
 
-            // Konvertáljuk az időket DateTime formátumba
             DateTime date1 = DateTime.ParseExact(time1, "HH:mm:ss", null);
             DateTime date2 = DateTime.ParseExact(time2, "HH:mm:ss", null);
 
-            // Időeltérés kiszámítása
             TimeSpan difference = date2 - date1;
 
-            // Az eltelt órák és percek
             Console.WriteLine($"Eltelt idő: {difference.Hours} óra és {difference.Minutes} perc.");
             Console.ReadKey();
         }
