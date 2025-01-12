242e47c47b353087571883851fc5c9962b62d4e6
Leviahegyrol9 <paplukacs.levi@gmail.com>
2025-01-12 21:56:00 +0100

    2025.01.12 - 21:55

--- Számítógépek_kiosztása/Program.cs
+++ Számítógépek_kiosztása/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using Function;
 
@@ -36,10 +37,6 @@ namespace Számítógépek_kiosztása
                 classAndCapacity[$"Terem{i}"] = capacity;
             }
 
-            int capacitySum = classAndCapacity.Sum(capacity => capacity.Value);
-
-            Console.WriteLine($"Összesen {capacitySum} gépnek van hely a termekben.");
-
             int minInvNum = Methods.GetNumberWithCondition("Kérem adja meg a leltárszám kezdetét: ", 0, int.MaxValue);
 
             List<Item> items = consoleServices.GetItems(minInvNum, inputPath, File.ReadAllLines(workersPath).ToHashSet(), classAndCapacity);           
@@ -47,11 +44,38 @@ namespace Számítógépek_kiosztása
             success = consoleServices.WriteFile(items.Where(item => item.Status == "Rendszergazda" | item.Status == "Tartalék").ToList(), rgPath);
             Methods.CheckSuccess(rgPath, success);
 
-            success = consoleServices.WriteFile(items.Where(item => item.Status.Contains("Terem")).ToList(), cPath);
+            List<Item> classItems = items.Where(item => item.Status.Contains("Terem")).ToList();
+
+            success = consoleServices.WriteFile(classItems, cPath);
             Methods.CheckSuccess(cPath, success);
 
             success = consoleServices.WriteFile(items.Where(item => item.Status.Contains("Dolgozó")).ToList(), wPath);
             Methods.CheckSuccess(wPath, success);
+
+            Thread.Sleep(2000);
+            Console.Clear();
+
+            int exchangeRate = Methods.GetNumberWithCondition("Kérem az árfolyamot: ", 1, int.MaxValue);
+
+            Console.WriteLine($"A termekben összesen {classItems.Count} eszköz található\nHUF: {classItems.Sum(classI => classI.Price):n2}\nEUR: {classItems.Sum(classI => classI.Price) / exchangeRate:n2}");
+
+            Console.Write("Kérem a gyári számot: ");
+
+            string serialNum = Console.ReadLine();
+
+            Item sN = items.Where(item => item.SerialNumber == serialNum).First();
+
+            Console.WriteLine($"Hol: {sN.Type}\nKinél: {sN.Status}");
+
+            Console.ReadKey();
+
+            Console.Write("Kérem a leltári számot: ");
+
+            string invNum = Console.ReadLine();
+
+            Item iN = items.Where(item => item.InvNumber == invNum).First();
+
+            Console.WriteLine($"Hol: {iN.Type}\nKinél: {iN.Status}");
 
             Console.ReadKey();
         }
