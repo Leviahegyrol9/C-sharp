b07e0ab144c280581553003099c6457323491b04
Leviahegyrol9 <paplukacs.levi@gmail.com>
2025-01-26 03:07:03 +0100

    2025.01.26 - 3:07

---
+++ WindowsFormsApp/Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace WindowsFormsApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnEnd = new System.Windows.Forms.Button();
+            this.result = new System.Windows.Forms.Label();
+            this.motorHP = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(56, 64);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(79, 43);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.start_Click);
+            // 
+            // btnEnd
+            // 
+            this.btnEnd.Location = new System.Drawing.Point(165, 64);
+            this.btnEnd.Name = "btnEnd";
+            this.btnEnd.Size = new System.Drawing.Size(79, 43);
+            this.btnEnd.TabIndex = 1;
+            this.btnEnd.Text = "Stop";
+            this.btnEnd.UseVisualStyleBackColor = true;
+            this.btnEnd.Click += new System.EventHandler(this.end_Click);
+            // 
+            // result
+            // 
+            this.result.AutoSize = true;
+            this.result.Location = new System.Drawing.Point(278, 68);
+            this.result.MinimumSize = new System.Drawing.Size(79, 43);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(79, 43);
+            this.result.TabIndex = 2;
+            // 
+            // motorHP
+            // 
+            this.motorHP.Location = new System.Drawing.Point(165, 26);
+            this.motorHP.Name = "motorHP";
+            this.motorHP.Size = new System.Drawing.Size(45, 22);
+            this.motorHP.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(103, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 16);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Motor %";
+            // 
+            // progressBar
+            // 
+            this.progressBar.Location = new System.Drawing.Point(106, 131);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(104, 28);
+            this.progressBar.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.motorHP);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.btnEnd);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnEnd;
+        private System.Windows.Forms.Label result;
+        private System.Windows.Forms.TextBox motorHP;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ProgressBar progressBar;
+    }
+}
+
---
+++ WindowsFormsApp/Form1.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    public partial class Form1 : Form
+    {
+        private DateTime startTime;
+        private DateTime endTime;
+        private bool isNumber;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void start_Click(object sender, EventArgs e)
+        {
+            startTime = DateTime.Now;
+
+            result.Text = string.Empty;
+            progressBar.Value = 0;
+
+            isNumber = GetMotorHp();
+            
+            if (isNumber)
+            {
+                progressBar.Value += 50;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void end_Click(object sender, EventArgs e)
+        {
+            endTime = DateTime.Now;
+
+            if (isNumber && motorHP.Text != string.Empty)
+            {
+                result.Text = $"Állapot: {motorHP.Text}%\nKi: {startTime.ToShortTimeString()}\nBe: {endTime.ToShortTimeString()}";
+                Clipboard.SetText(result.Text);
+                motorHP.Text = string.Empty;
+                progressBar.Value += 50;
+            }
+        }
+
+        private bool GetMotorHp()
+        {
+            bool isNumber = int.TryParse(motorHP.Text, out int value);
+
+            if (!isNumber)
+            {
+                result.Text = "Nem számot adtál meg!";
+                return false;
+            }
+            else if (value < 0 || value > 100)
+            {
+                result.Text = "0 és 100 között add meg!";
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
+        }
+    }
+}
