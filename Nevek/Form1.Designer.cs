04b516d72ba2ffdfad27fa940af73c81221475d5
Leviahegyrol69 <paplukacs.levi@gmail.com>
2025-02-05 12:30:55 +0100

    2025.02.05 - 12:30

---
+++ Nevek/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿namespace Nevek
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.namesCb = new System.Windows.Forms.ComboBox();
+            this.resultCb = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.result = new System.Windows.Forms.Label();
+            this.readFileBtn = new System.Windows.Forms.Button();
+            this.resultBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // namesCb
+            // 
+            this.namesCb.DropDownStyle = System.Windows.Forms.ComboBoxStyle.Simple;
+            this.namesCb.FormattingEnabled = true;
+            this.namesCb.Location = new System.Drawing.Point(79, 95);
+            this.namesCb.Name = "namesCb";
+            this.namesCb.Size = new System.Drawing.Size(121, 189);
+            this.namesCb.TabIndex = 0;
+            // 
+            // resultCb
+            // 
+            this.resultCb.DropDownStyle = System.Windows.Forms.ComboBoxStyle.Simple;
+            this.resultCb.FormattingEnabled = true;
+            this.resultCb.Location = new System.Drawing.Point(473, 95);
+            this.resultCb.Name = "resultCb";
+            this.resultCb.Size = new System.Drawing.Size(121, 189);
+            this.resultCb.TabIndex = 1;
+            this.resultCb.Visible = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(76, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Összes";
+            // 
+            // result
+            // 
+            this.result.AutoSize = true;
+            this.result.Location = new System.Drawing.Point(470, 54);
+            this.result.Name = "result";
+            this.result.Size = new System.Drawing.Size(0, 13);
+            this.result.TabIndex = 3;
+            // 
+            // readFileBtn
+            // 
+            this.readFileBtn.Location = new System.Drawing.Point(216, 49);
+            this.readFileBtn.Name = "readFileBtn";
+            this.readFileBtn.Size = new System.Drawing.Size(75, 23);
+            this.readFileBtn.TabIndex = 4;
+            this.readFileBtn.Text = "Beolvasás";
+            this.readFileBtn.UseVisualStyleBackColor = true;
+            this.readFileBtn.Click += new System.EventHandler(this.readFileBtn_Click);
+            // 
+            // resultBtn
+            // 
+            this.resultBtn.Location = new System.Drawing.Point(623, 49);
+            this.resultBtn.Name = "resultBtn";
+            this.resultBtn.Size = new System.Drawing.Size(75, 23);
+            this.resultBtn.TabIndex = 5;
+            this.resultBtn.Text = "Előfordulás";
+            this.resultBtn.UseVisualStyleBackColor = true;
+            this.resultBtn.Click += new System.EventHandler(this.resultBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.resultBtn);
+            this.Controls.Add(this.readFileBtn);
+            this.Controls.Add(this.result);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.resultCb);
+            this.Controls.Add(this.namesCb);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox namesCb;
+        private System.Windows.Forms.ComboBox resultCb;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label result;
+        private System.Windows.Forms.Button readFileBtn;
+        private System.Windows.Forms.Button resultBtn;
+    }
+}
+
---
+++ Nevek/Form1.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Nevek
+{
+    public partial class Form1 : Form
+    {
+        const string path = "nevek.txt";
+        List<string> allNames = new List<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void readFileBtn_Click(object sender, EventArgs e)
+        {
+            resultCb.Visible = false;
+            result.Text = string.Empty;
+
+            List<string> names = ReadFile(path);
+
+            foreach (string name in names)
+            {
+                namesCb.Items.Add(name);
+            }
+
+        }
+        private List<string> ReadFile(string path)
+        {
+            try
+            {
+                allNames = File.ReadAllLines(path).ToList();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            return allNames;
+        }
+
+        private void resultBtn_Click(object sender, EventArgs e)
+        {
+            Dictionary<string, int> namesAndCount = GetNamesAndCount(allNames);
+
+            if (namesAndCount.Count != 0)
+            {
+                foreach (string name in namesAndCount.Keys)
+                {
+                    resultCb.Items.Add(name);
+                }
+
+                resultCb.Visible = true;
+
+                result.Text = $"Egyedi: {namesAndCount.Count} db.";
+            }
+        }
+
+        private Dictionary<string, int> GetNamesAndCount(List<string> allNames)
+        {
+            Dictionary<string, int> namesAndCount = new Dictionary<string, int>();
+            HashSet<string> names = GetNames(allNames);
+
+            foreach (string name in names)
+            {
+                namesAndCount[name] = 0;
+            }
+
+            foreach (string name in allNames)
+            {
+                namesAndCount[name]++;
+            }
+
+            Dictionary<string, int> sorted = namesAndCount.OrderBy(name => name.Key).ToDictionary(k => k.Key, v => v.Value);
+
+            return sorted;
+        }
+
+        private HashSet<string> GetNames(List<string> allNames)
+        {
+            HashSet<string> names = new HashSet<string>();
+
+            foreach (string name in allNames)
+            {
+                names.Add(name);
+            }
+
+            return names;
+        }
+    }
+}
