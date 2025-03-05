3acad6ae6eaa715213c0baea9dac4ecf547c0842
Leviahegyrol9 <paplukacs.levi@gmail.com>
2025-03-05 17:37:42 +0100

    2025.03.05 - 17:37

---
+++ Verseny/TimeIO.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Verseny
+{
+    public class TimeIO
+    {
+        public static List<Time> GetTImes(string path)
+        {
+            List<Time> times = new List<Time>();
+            Time time = null;
+
+            try
+            {
+                string[] lines = File.ReadAllLines(path);
+
+                foreach (string line in lines)
+                {
+                    string[] datas = line.Split(' ');
+
+                    time = new Time
+                    {
+                        Hour = int.Parse(datas[0]),
+                        Minute = int.Parse(datas[1])
+                    };
+
+                    times.Add(time);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.ReadKey();
+                Environment.Exit(0);
+            }
+
+            return times;
+        }
+    }
+}
