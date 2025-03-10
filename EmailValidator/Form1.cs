7820cffaaab8db16dd3430f031c73a5c8d1dfbc4
Leviahegyrol69 <paplukacs.levi@gmail.com>
2025-03-10 14:45:31 +0100

    2025.03.10 - 14:45

---
+++ EmailValidator/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EmailValidator
+{
+    public partial class Form1 : Form
+    {
+        string emailPattern = RegexPattern.emailPattern;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void loginBtn_Click(object sender, EventArgs e)
+        {     
+            if (Regex.IsMatch(email.Text, emailPattern))
+            {
+                MessageBox.Show("Valid email!", "Email validator", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Unvalid email!", "Email validator", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                email.Clear();
+            }          
+        }
+    }
+}
---
+++ PictureBoxok megkeverése/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PictureBoxok_megkeverése
+{
+    public partial class Form1 : Form
+    {
+        private PictureBox[] pictureBoxes;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if(pictureBoxes == null || pictureBoxes.Length == 0)
+            {
+                return;
+            }
+
+            Random rnd = new Random();
+
+            List<Image> images = pictureBoxes.Select(img => img.Image).ToList();
+            images = images.OrderBy(_ => rnd.Next()).ToList();
+
+            for(int i = 0; i < pictureBoxes.Length; i++)
+            {
+                pictureBoxes[i].Image = images[i];
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            pictureBoxes = this.Controls.OfType<PictureBox>().ToArray();
+        }
+    }
+}
