574b798c24ce2f4b4dc8744d2e6c7f85f3dc1f6a
Leviahegyrol9 <paplukacs.levi@gmail.com>
2025-04-08 17:42:13 +0200

    2025.04.08 - 17:42

---
+++ Git/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            int number;
+            bool isNumber;
+
+            do
+            {
+                Console.Write("1 - Git pull\n2 - Git push\nVálasztás: ");
+                string input = Console.ReadLine();
+
+                isNumber = int.TryParse(input, out number);
+
+                if (!isNumber)
+                {
+                    Console.WriteLine("Nem számot adott meg!");
+                    await Task.Delay(2000);
+                }
+                else if (number > 2 || number < 1)
+                {
+                    Console.WriteLine("Nincs ilyen opció!");
+                    await Task.Delay(2000);
+                }
+
+                Console.Clear();
+
+            } while (!isNumber || number > 2 || number < 1);
+
+            switch (number)
+            {
+                case 1:
+                    break;
+
+                case 2:
+                    break;
+            }
+        }
+    }
+}
