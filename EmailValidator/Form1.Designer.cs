31174c395bc81aa7a0717c4b8f038ef477f4d180
Leviahegyrol69 <paplukacs.levi@gmail.com>
2025-05-12 10:17:04 +0200

    2025.05.12 - 10:16

--- EmailValidator/Form1.Designer.cs
+++ EmailValidator/Form1.Designer.cs
@@ -31,11 +31,15 @@
             this.email = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.loginBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.ageTb = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.nameTb = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // email
             // 
-            this.email.Location = new System.Drawing.Point(93, 49);
+            this.email.Location = new System.Drawing.Point(402, 40);
             this.email.Name = "email";
             this.email.Size = new System.Drawing.Size(100, 20);
             this.email.TabIndex = 0;
@@ -43,7 +47,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(12, 52);
+            this.label1.Location = new System.Drawing.Point(321, 43);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(75, 13);
             this.label1.TabIndex = 1;
@@ -51,7 +55,7 @@
             // 
             // loginBtn
             // 
-            this.loginBtn.Location = new System.Drawing.Point(208, 49);
+            this.loginBtn.Location = new System.Drawing.Point(376, 134);
             this.loginBtn.Name = "loginBtn";
             this.loginBtn.Size = new System.Drawing.Size(59, 20);
             this.loginBtn.TabIndex = 2;
@@ -60,11 +64,47 @@
             this.loginBtn.UseVisualStyleBackColor = true;
             this.loginBtn.Click += new System.EventHandler(this.loginBtn_Click);
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(341, 95);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(29, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Age:";
+            // 
+            // ageTb
+            // 
+            this.ageTb.Location = new System.Drawing.Point(402, 92);
+            this.ageTb.Name = "ageTb";
+            this.ageTb.Size = new System.Drawing.Size(100, 20);
+            this.ageTb.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(341, 69);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(38, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Name:";
+            // 
+            // nameTb
+            // 
+            this.nameTb.Location = new System.Drawing.Point(402, 66);
+            this.nameTb.Name = "nameTb";
+            this.nameTb.Size = new System.Drawing.Size(100, 20);
+            this.nameTb.TabIndex = 5;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.nameTb);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.ageTb);
             this.Controls.Add(this.loginBtn);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.email);
@@ -80,6 +120,10 @@
         private System.Windows.Forms.TextBox email;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button loginBtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox ageTb;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox nameTb;
     }
 }
 
--- EmailValidator/Form1.cs
+++ EmailValidator/Form1.cs
@@ -6,30 +6,39 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml.Linq;
 
 namespace EmailValidator
 {
     public partial class Form1 : Form
     {
-        string emailPattern = RegexPattern.emailPattern;
         public Form1()
         {
             InitializeComponent();
         }
 
         private void loginBtn_Click(object sender, EventArgs e)
-        {     
-            if (Regex.IsMatch(email.Text, emailPattern))
+        {
+            bool isString = nameTb.Text.Replace(" ", "").All(char.IsLetter);
+
+            if (!isString || nameTb.Text.Length < 3)
             {
-                MessageBox.Show("Valid email!", "Email validator", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MessageBox.Show("Nem nevet adott meg!","Hiba", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-            else
+
+            bool isNumber = int.TryParse(ageTb.Text, out int age);
+
+            if (!isNumber || age > 150 || age < 0)
             {
-                MessageBox.Show("Unvalid email!", "Email validator", MessageBoxButtons.OK, MessageBoxIcon.Error);
-                email.Clear();
-            }          
+                MessageBox.Show("Nem életkort adott meg!", "Hiba", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+            bool isValidEmail = Regex.IsMatch(email.Text, RegexPattern.emailPattern);
+
+            if (!isValidEmail) MessageBox.Show("Nem emailt adott meg!", "Hiba", MessageBoxButtons.OK, MessageBoxIcon.Error);
         }
     }
 }
