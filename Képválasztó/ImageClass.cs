4078d0541913c9000e448e07e5f96066831aff40
Leviahegyrol69 <paplukacs.levi@gmail.com>
2025-05-26 08:07:35 +0200

    2025.05.26 - 8:07

---
+++ Képválasztó/ImageClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Képválasztó
+{
+    public class ImageClass
+    {
+        public string Source { get; set; }
+
+        public string Name => Path.GetFileNameWithoutExtension(Source);
+
+        public ImageClass()
+        {
+            
+        }
+    }
+}
