9914168517df564863419a333fbe3048048504a5
Leviahegyrol69 <paplukacs.levi@gmail.com>
2025-09-24 10:39:53 +0200

    2025.09.24 - 10:39

---
+++ Röplabda/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Röplabda
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const string fileName = "roplabda.txt"; 
+        }
+
+        private static List<Team> GetTeams(string fileName)
+        {
+            List<Team> teams = new List<Team>();
+            Team team = null;
+
+            string[] lines = File.ReadAllLines(fileName);
+
+            foreach (string line in lines)
+            {
+                string[] datas = line.Split('\t');
+
+                List<int> points = new List<int>();
+
+                foreach (string point in datas[1].Split(','))
+                {
+                    points.Add(int.Parse(point));
+                }
+
+                team = new Team
+                {
+                    TeamName = datas[0],
+                    Points = new List<int>().AddRange(points)
+                };
+            }
+        }
+
+    }
+}
