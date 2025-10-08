82887d3d3e42f5307b79c6591d11fda29aebcbd8
Leviahegyrol69 <paplukacs.levi@gmail.com>
2025-10-08 10:40:15 +0200

    2025.10.08 - 10:40

---
+++ Telekocsi/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telekocsi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Car> cars = GetCars("autok.csv");
+        }
+        private static List<Car> GetCars(string path)
+        {
+            List<Car> cars = new List<Car>();
+            Car car = null;
+        }
+    }
+}
