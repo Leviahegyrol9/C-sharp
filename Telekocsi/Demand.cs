785e32501a24a0670c2ebfdabfb58bb1616932a9
Leviahegyrol69 <paplukacs.levi@gmail.com>
2025-10-08 14:20:32 +0200

    2025.10.08 - 14:20

---
+++ Telekocsi/Demand.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Telekocsi
+{
+    public class Demand
+    {
+        public string Id {  get; set; }
+        public string Start {  get; set; }
+        public string End {  get; set; }
+        public int People {  get; set; }
+        public Demand()
+        {
+            
+        }
+    }
+}
