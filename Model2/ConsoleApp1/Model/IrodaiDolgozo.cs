41d0737990c89ec44422f3c94154b1fb2472ac45
Leviahegyrol69 <paplukacs.levi@gmail.com>
2025-11-12 09:39:06 +0100

    2025.11.12 - 09:38

---
+++ Model2/ConsoleApp1/Model/IrodaiDolgozo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1.Model
+{
+    public class IrodaiDolgozo:Dolgozo
+    {
+        public string osztaly {  get; set; }
+
+        public IrodaiDolgozo(string nev,string azonosito,int ber,string osztaly):base(nev,azonosito,ber)
+        {
+            this.osztaly = osztaly;
+        }
+
+        public override void KiirInformacio()
+        {
+            base.KiirInformacio();
+            Console.WriteLine("Osztály:"+osztaly);
+        }
+        public void Dolgozik()
+        {
+            Console.WriteLine(nev+" adminisztratív feladatokat lát el "+osztaly+" osztályon.");
+        }
+    }
+}
---
+++ Model2/ConsoleApp1/Program.cs
@@ -0,0 +1,36 @@
+﻿using ConsoleApp1.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Dolgozo> lista = new List<Dolgozo>
+            {
+                new IrodaiDolgozo("Kis Péter","ID123",450000,"Pénzügy"),
+                new IrodaiDolgozo("Nagy Péter","ID234",550000,"Számvitel"),
+                new FizikaiDolgozo("Tóth Tamás","ID456",670000,"nappali"),
+                new FizikaiDolgozo("Tóth Tibor","ID678",770000,"éjszakai"),
+            };
+            foreach(var i in lista)
+            {
+                i.KiirInformacio();
+                if(i is IrodaiDolgozo irodai)
+                {
+                    irodai.Dolgozik();
+                }
+                if(i is FizikaiDolgozo fizikai)
+                {
+                    fizikai.Dolgozik();
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
