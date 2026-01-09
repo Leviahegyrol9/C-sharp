1f56bba07c8a3dca90d5f267596547aeb317ab70
Leviahegyrol69 <paplukacs.levi@gmail.com>
2026-01-09 10:11:24 +0100

    2026.01.09 - 10:11

---
+++ WpfApp2/WpfApp2/UserControls/Jegyzetiras.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2.UserControls
+{
+    /// <summary>
+    /// Interaction logic for Jegyzetiras.xaml
+    /// </summary>
+    public partial class Jegyzetiras : UserControl
+    {
+        public static string mentettJegyzet = string.Empty;
+        public Jegyzetiras()
+        {
+            InitializeComponent();
+        }
+
+        private void mentes_Click(object sender, RoutedEventArgs e)
+        {
+            mentettJegyzet = jegyzet.Text;
+            File.AppendAllText("jegyzetek.txt", jegyzet.Text + "\n");
+            MessageBox.Show("Sikeres mentés!", "Mentés", MessageBoxButton.OK, MessageBoxImage.Information);
+
+        }
+    }
+}
---
+++ WpfApp2/WpfApp2/UserControls/Megtekintes.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2.UserControls
+{
+    /// <summary>
+    /// Interaction logic for Megtekintes.xaml
+    /// </summary>
+    public partial class Megtekintes : UserControl
+    {
+        public Megtekintes()
+        {
+            InitializeComponent();
+            //mentettjegyzet.Text = Jegyzetiras.mentettJegyzet;
+            if (File.Exists("jegyzetek.txt"))
+            {
+                mentettjegyzet.Text = File.ReadAllText("jegyzetek.txt");
+            }
+            else
+            {
+                mentettjegyzet.Text = "Nincs még mentett jegyzet";
+            }
+        }
+    }
+}
