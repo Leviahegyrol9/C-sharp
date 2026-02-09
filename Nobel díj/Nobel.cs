8099180328b76ea8b67e4b9e34573b1358e7a9be
Leviahegyrol69 <paplukacs.levi@gmail.com>
2026-02-09 10:02:42 +0100

    2026.02.09 - 10:02

---
+++ Nobel díj/Nobel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nobel_díj
+{
+    public class Nobel
+    {
+        public int evszam {  get; set; }
+        public string tipus {  get; set; }
+        public string keresztnev {  get; set; }
+        public string vezeteknev {  get; set; }
+
+        public override string ToString()
+        {
+            return $"{evszam};{tipus};{keresztnev};{vezeteknev}";
+        }
+    }
+}
