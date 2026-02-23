5b9658dc0e13ded5e6a4e151ea22e3677392f76b
Leviahegyrol69 <paplukacs.levi@gmail.com>
2026-02-23 09:58:39 +0100

    2026.02.23 - 09:58

---
+++ Etterem/Etterem/UserControls/Statisztika.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Etterem.UserControls
+{
+    /// <summary>
+    /// Interaction logic for Statisztika.xaml
+    /// </summary>
+    public partial class Statisztika : Window
+    {
+        public Statisztika()
+        {
+            InitializeComponent();
+
+            Dictionary<string, int> stat = MainWindow.vendegek.GroupBy(x => x.Tipus).ToDictionary(x => x.Key, y => y.Count());
+
+            foreach (var item in stat) lista.Items.Add($"{item.Key} - {item.Value}");
+        }
+    }
+}
---
+++ Etterem/Etterem/UserControls/etel stisztika.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Etterem.UserControls
+{
+    /// <summary>
+    /// Interaction logic for etel_stisztika.xaml
+    /// </summary>
+    public partial class etel_stisztika : Window
+    {
+        public etel_stisztika()
+        {
+            InitializeComponent();
+            var stat = MainWindow.vendegek.GroupBy(x => x.EtelFajta).ToDictionary(x => x.Key, y => y.Count());
+
+            foreach (var item in stat) lista2.Items.Add($"{item.Key} - {item.Value}");
+
+            vegosszeg.Text = MainWindow.vendegek.Sum(x => x.Osszeg).ToString();
+        }
+    }
+}
