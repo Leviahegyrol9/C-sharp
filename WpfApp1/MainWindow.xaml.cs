cd0ff4622fecb78db6b7c44f9dbc1dbb5a0360a8
Leviahegyrol69 <paplukacs.levi@gmail.com>
2026-03-02 10:03:56 +0100

    2026.03.02 - 10:03

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using WpfApp1;
 
 namespace WpfApp1
 {
@@ -20,19 +23,58 @@ namespace WpfApp1
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Konyvek> list = new List<Konyvek>();
         public MainWindow()
         {
             InitializeComponent();
+            foreach (var i in File.ReadAllLines("konyvek.txt"))
+            {
+                list.Add(new Konyvek(i));
+            }
         }
 
-        private void SliderChange(object sender, RoutedPropertyChangedEventArgs<double> e)
+        private void legdragabb_Click(object sender, RoutedEventArgs e)
         {
-            slider1.Text = $"Slider value: {e.NewValue}";
+            lista.Items.Clear();
+            var maxra = list.OrderBy(x => x.ar).Last();
+            lista.Items.Add($"{maxra.veznev}\t{maxra.kernev}\t{maxra.konyvcim}");
         }
 
-        private void SliderChange2(object sender, RoutedPropertyChangedEventArgs<double> e)
+        private void termtud_Click(object sender, RoutedEventArgs e)
         {
-            slider2.Text = $"Slider value: {e.NewValue}";
+            lista.Items.Clear();
+            var teremtud = list.Where(x => x.tema == "természerttudomány");
+        }
+
+        private void osszeg_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void konyvstat_Click(object sender, RoutedEventArgs e)
+        {
+            lista.Items.Clear();
+            var stat = list.GroupBy(x => x.kiadaseve).ToDictionary(x => x.Key, y => y.Count());
+            foreach (var i in stat)
+            {
+                lista.Items.Add(i.Key + " - " + i.Value);
+            }
+        }
+
+        private void maxhon_Click(object sender, RoutedEventArgs e)
+        {
+            lista.Items.Clear();
+            var maxh = list.OrderBy(x => x.honorarium).Last();
+            lista.Items.Add(maxh.veznev + '\t' + maxh.kernev + '\t' + maxh.konyvcim);
+        }
+
+        private void betoltes_Click(object sender, RoutedEventArgs e)
+        {
+            foreach (var i in list)
+            {
+                lista.Items.Add(i.veznev + '\t' + i.kernev + '\t' + i.konyvcim);
+            }
+            lista.Items.Add("Konyvek szama: " + list.Count);
         }
     }
 }
