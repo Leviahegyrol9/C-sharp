968d86aed0946fef4358899f9e14f0b57605b7e9
Leviahegyrol69 <paplukacs.levi@gmail.com>
2026-03-06 11:47:40 +0100

    2026.03.06 - 11:47

---
+++ WpfApp1/WpfApp1/Kereskedelmiceg.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp1
+{
+    public class Kereskedelmiceg:Ceg
+    {
+      
+
+        public int uzletszam {  get; set; }
+        public int vevoszam {  get; set; }
+
+        public Kereskedelmiceg(string nev, int alkamazottszam, int bevetel,int uzletszam,int vevoszam) : base(nev, alkamazottszam, bevetel)
+        {
+            this.uzletszam = uzletszam;
+            this.vevoszam = vevoszam;
+        }
+        public override int Arany()
+        {
+            return vevoszam / uzletszam;
+        }
+
+        public override string ToString()
+        {
+            return "Egy üzletre juto vevoszam:" + Arany().ToString("f2");
+        }
+    }
+
+}
---
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            rbgyarto.Checked += Rb_Checked;
+            rbalap.Checked += Rb_Checked;
+            rbkereskedlmi.Checked += Rb_Checked;
+
+        }
+
+        private void Rb_Checked(object sender, RoutedEventArgs e)
+        {           
+            foreach (GroupBox groupB in mains.Children.OfType<GroupBox>())
+            {
+                groupB.Visibility = Visibility.Collapsed;
+            }
+
+            GroupBox gB = sender as GroupBox;
+
+            gB.Visibility = Visibility.Visible;
+
+
+        }
+
+        private void szamitas_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
