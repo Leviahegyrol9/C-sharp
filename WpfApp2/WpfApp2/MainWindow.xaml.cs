707b53947bf3f2b2be50a3b65b219cfe6491319a
Leviahegyrol69 <paplukacs.levi@gmail.com>
2026-03-26 12:01:13 +0100

    2026.03.26 - 12:01

---
+++ WpfApp2/WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Product> Listproduct {  get; set; }
+        public Product Newproduct { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Listproduct=new List<Product>();
+            Newproduct = new Product();
+            this.DataContext = this;
+        }
+
+        private void hozzaad_Click(object sender, RoutedEventArgs e)
+        {
+            Listproduct.Add(new Product(Newproduct.Name,Newproduct.Price));
+            Newproduct=new Product();
+            this.DataContext = null;
+            this.DataContext = this;
+        }
+    }
+}
---
+++ WpfApp2/WpfApp2/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp2
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public int Price {get; set; }
+
+        public Product(string name, int price)
+        {
+            Name = name;
+            Price = price;
+        }
+
+        public Product()
+        {
+        }
+
+        public override string ToString()
+        {
+            return Name + " - " + Price + " Ft";
+        }
+    }
+}
