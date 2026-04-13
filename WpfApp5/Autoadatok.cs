4ecc356c22f791b4e17db9bf4b904d8775e866ae
Leviahegyrol69 <paplukacs.levi@gmail.com>
2026-04-13 09:49:48 +0200

    2026.04.13 - 09:49

---
+++ WpfApp5/Autoadatok.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp5
+{
+    public class Autoadatok: INotifyPropertyChanged
+    {
+        public double tav = 1;
+        public double uzemanyag = 1;
+        public double fogyasztas;
+
+        public double Tav
+        {
+            get
+            {
+                return tav;
+            }
+            set
+            {
+                tav = value;
+                OnPropertychanged("Fogyasztas");
+            }
+        }
+
+        public void OnPropertychanged(string v)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(v));
+            }
+        }
+
+        public double Uzemanyag
+        {
+            get
+            {
+                return uzemanyag;
+            }
+            set
+            {
+                uzemanyag = value;
+            }
+        }
+
+        public double Fogyasztas
+        {
+            get
+            {
+                fogyasztas = (uzemanyag / tav) * 100;
+                return Math.Round(fogyasztas, 2 );
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
---
+++ WpfApp6/Adatok.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp6
+{
+    public class Adatok : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public int beszerzesi = 10;
+        public int eladasi = 100;
+        public int profit;
+
+        public int Beszerzesi
+        {
+            get
+            {
+                return beszerzesi;
+            }
+            set
+            {
+                beszerzesi = value;
+                OnPropertyChanged("Profit");
+            }
+        }
+
+        public void OnPropertyChanged(string v)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(v));
+            }
+        }
+
+        public int Eladasi
+        {
+            get
+            {
+                return eladasi;
+            }
+            set
+            {
+                eladasi = value;
+            }
+        }
+
+        public int Profit
+        {
+            get
+            {
+                return eladasi - beszerzesi;
+            }
+        }
+    }
+}
