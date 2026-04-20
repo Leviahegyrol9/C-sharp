3ce9fed5f6e38823a3901d21dfe29a2d6a26bd9e
Leviahegyrol69 <paplukacs.levi@gmail.com>
2026-04-20 09:34:09 +0200

    2026.04.20 - 09:34

---
+++ WpfApp10/Viz.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp10
+{
+    public class Viz:INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public double elozo;
+        public double aktualis;
+        public double vizdij;
+        public double koltseg;
+        public double Elozo
+        {
+            get { return elozo; }
+            set { elozo = value; OnPropertyChanged("Koltseg"); }
+        }
+        public double Aktualis
+        {
+            get { return aktualis; }
+            set { aktualis = value; OnPropertyChanged("Koltseg"); }
+        }
+        public double Vizdij
+        {
+            get { return vizdij; }
+            set { vizdij = value; OnPropertyChanged("Koltseg"); }
+        }
+
+        private void OnPropertyChanged(string v)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(v));
+            }
+        }
+
+        public double Koltseg
+        {
+            get
+            {
+                return (aktualis - elozo) * vizdij;
+            }
+        }
+    }
+}
