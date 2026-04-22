52d687093215bd70d699266d235209f188d39027
Leviahegyrol69 <paplukacs.levi@gmail.com>
2026-04-22 14:22:48 +0200

    2026.04.22 - 14:22

---
+++ Berek/Ber.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Berek
+{
+    public class Ber:INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public double brutto;
+        public double levonasok;
+        public double netto;
+        public double osszeg;
+
+        public double Brutto
+        {
+            get {  return brutto; }
+            set { brutto = value; OnPropertyChanged("Netto", "Osszeg"); }
+        }
+        public double Levonasok
+        {
+            get {  return levonasok; }
+            set { levonasok = value; OnPropertyChanged("Netto","Osszeg"); }
+        }
+
+        private void OnPropertyChanged(string v, string v2)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(v));
+                PropertyChanged(this, new PropertyChangedEventArgs(v2));
+            }
+        }
+
+        public double Netto
+        {
+            get
+            {
+                return Brutto - Osszeg;
+            }
+        }
+        public double Osszeg
+        {
+            get
+            {
+                return Brutto * (Levonasok / 100);
+            }
+        }
+
+
+    }
+}
