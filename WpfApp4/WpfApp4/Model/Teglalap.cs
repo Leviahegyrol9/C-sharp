586466cc628651f8d10602f652a221fdd3586ba5
Leviahegyrol69 <paplukacs.levi@gmail.com>
2026-05-15 11:52:53 +0200

    2026.05.15 - 11:52

---
+++ WpfApp4/WpfApp4/Model/Teglalap.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp4.Model
+{
+    public class Teglalap
+    {
+        public double aoldal;
+        public double boldal;
+        public double terulet;
+        public double kerulet;
+        public double atlo;
+
+        public double AOldal
+        {
+            get { return aoldal; }
+            set { aoldal = value; }
+        }
+        public double BOldal
+        {
+            get { return boldal; }
+            set { boldal = value; }
+        }
+        public double Terulet
+        {
+            get
+            {
+                terulet = (aoldal * boldal);
+                return terulet;
+            }
+        }
+        public double Kerulet
+        {
+            get { 
+                kerulet = 2 * (aoldal + boldal);
+                return kerulet; 
+            }
+        }
+        public double Atlo
+        {
+            get {
+                atlo = Math.Sqrt(Math.Pow(aoldal, 2) + Math.Pow(atlo, 2));
+                return atlo;
+            }
+        }
+    }
+}
---
+++ WpfApp4/WpfApp4/ViewModel/TeglalapViewModel.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Documents;
+using ViewModels.BaseClass;
+using WpfApp4.Model;
+
+namespace WpfApp4.ViewModel
+{
+    public class TeglalapViewModel:ViewModelBase
+    {
+        public Teglalap t;
+        public RelayCommand CalculateCommand { get; set; }
+        public RelayCommand CloseCommand { get;set; }
+
+
+        public TeglalapViewModel()
+        {
+            t = new Teglalap();
+            CalculateCommand = new RelayCommand(execute => Calculate());
+            CloseCommand = new RelayCommand(execute => CloseWindow());
+        }
+
+        public double AOldal
+        {
+            get { return t.aoldal; }
+            set { t.aoldal = value; }
+        }
+        public double BOldal
+        {
+            get { return t.boldal; }
+            set { t.boldal = value; }
+        }
+        public double Terulet
+        {
+            get { return t.Terulet; }
+
+        }
+        public double Kerulet
+        {
+            get
+            {
+                return t.Kerulet;
+            }
+        }
+        public double Atlo
+        {
+            get
+            {
+                return t.Atlo;
+            }
+        }
+        public void Calculate()
+        {
+            OnPropertyChanged("Terulet");
+            OnPropertyChanged("Kerulet");
+            OnPropertyChanged("Atlo");
+        }
+        public void CloseWindow()
+        {
+            Application.Current.Shutdown();
+        }
+    }
+}
