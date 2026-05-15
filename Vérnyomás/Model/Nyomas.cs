6cff9f5cdfed3a9ccaab09af224c59561570b123
Leviahegyrol69 <paplukacs.levi@gmail.com>
2026-05-15 13:08:55 +0200

    2026.05.15 - 13:08

---
+++ Vérnyomás/Model/Nyomas.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vérnyomás.Model
+{
+    public class Nyomas
+    {
+        public double kor;
+        public double suly;
+        public double szint;
+        public double pulzus;
+        public string vernyomas;
+
+        public double Kor
+        {
+            get {  return kor; }
+            set { kor = value; }
+        }
+        public double Suly
+        {
+            get { return suly; }
+            set { suly = value; }
+        }
+        public double Szint
+        {
+            get { return szint; }
+            set { szint = value; }
+        }
+        public double Pulzus
+        {
+            get 
+            {
+                return 220 - kor * 0.7;
+            }
+        }
+        public string Vernyomas
+        {
+            get
+            {
+                double bal = 120 + ((kor - 20) / 10) * 2 + ((suly - 70) / 10) * 1.5 + szint * 5;
+                double jobb = 80 + ((kor - 20) / 10) * 1.5 + ((suly - 70) / 10) * 1 + szint * 3;
+                return $"{Math.Round(bal, 0)}/{Math.Round(jobb)}";
+            }
+        }
+    }
+}
---
+++ Vérnyomás/ViewModel/NyomasViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Vérnyomás.Model;
+using ViewModels.BaseClass;
+
+namespace Vérnyomás.ViewModel
+{
+    public class NyomasViewModel:ViewModelBase
+    {
+        public Nyomas n;
+        public RelayCommand CalculateCommand { get; set; }
+
+        public NyomasViewModel()
+        {
+            n = new Nyomas();
+            CalculateCommand = new RelayCommand(execute => Calculate());           
+        }
+
+        public void Calculate()
+        {
+            OnPropertyChanged("Pulzus");
+            OnPropertyChanged("Vernyomas");
+        }
+
+        public double Kor
+        {
+            get { return n.kor; }
+            set { n.kor = value; }
+        }
+        public double Suly
+        {
+            get { return n.suly; }
+            set { n.suly = value; }
+        }
+        public double Szint
+        {
+            get { return n.szint; }
+            set { n.szint = value; }
+        }
+        public double Pulzus
+        {
+            get
+            {
+                return n.Pulzus;
+            }
+        }
+        public string Vernyomas
+        {
+            get
+            {
+                return n.Vernyomas;
+            }
+        }     
+    }
+}
