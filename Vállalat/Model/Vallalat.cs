5442b0355c31b5eb18089563e0e02bc79596166a
Leviahegyrol69 <paplukacs.levi@gmail.com>
2026-05-21 08:49:49 +0200

    2026.05.21 - 08:49

---
+++ Vállalat/Model/Vallalat.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vállalat.Model
+{
+    public class Vallalat
+    {
+        public double bevetel;
+        public double egyeb;
+        public double bere;
+        public double koltsegek;
+        public double oBevetel;
+        public double oKoltseg;
+        public double aElotti;
+        public double ado;
+        public double aEredmeny;
+
+        public double Bevetel
+        {
+            get {  return bevetel; }
+            set { bevetel = value; }
+        }
+        public double Egyeb
+        {
+            get { return egyeb; }
+            set { egyeb = value; }
+        }
+        public double Bere
+        {
+            get { return bere; }
+            set { bere = value; }
+        }
+        public double Koltsegek
+        {
+            get { return koltsegek; }
+            set { koltsegek = value; }
+        }
+        public double OBevetel
+        {
+            get 
+            {
+                oBevetel = bevetel + egyeb;
+                return oBevetel;
+            }
+        }
+        public double OKoltseg
+        {
+            get
+            {
+                oKoltseg = bere + koltsegek;
+                return oKoltseg;
+            }
+        }
+        public double AElotti
+        {
+            get
+            {
+                return oBevetel - oKoltseg;
+            }
+        }
+        public double Ado
+        {
+            get
+            {
+                ado = AElotti * 0.3;
+                return ado;
+            }
+        }
+        public double AEredmeny
+        {
+            get
+            {
+                aEredmeny = AElotti - ado;
+                return aEredmeny;
+            }
+        }
+    }
+}
---
+++ Vállalat/ViewModel/VallalatViewModel.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using Vállalat.Model;
+using ViewModels;
+using ViewModels.BaseClass;
+
+namespace Vállalat.ViewModel
+{
+    public class VallalatViewModel:ViewModelBase
+    {
+        public Vallalat v = new Vallalat();
+
+        public RelayCommand CalculateCommand { get; set; }
+
+        public VallalatViewModel()
+        {
+            CalculateCommand = new RelayCommand(execute => Calculate());
+        }
+
+        public void Calculate()
+        {
+            OnPropertyChanged("OBevetel");
+            OnPropertyChanged("OKoltseg");
+            OnPropertyChanged("AElotti");
+            OnPropertyChanged("Ado");
+            OnPropertyChanged("AEredmeny");
+        }
+
+        public double Bevetel
+        {
+            get { return v.bevetel; }
+            set { v.bevetel = value; }
+        }
+        public double Egyeb
+        {
+            get { return v.egyeb; }
+            set { v.egyeb = value; }
+        }
+        public double Bere
+        {
+            get { return v.bere; }
+            set { v.bere = value; }
+        }
+        public double Koltsegek
+        {
+            get { return v.koltsegek; }
+            set { v.koltsegek = value; }
+        }
+        public double OBevetel
+        {
+            get
+            {             
+                return v.OBevetel;
+            }
+        }
+        public double OKoltseg
+        {
+            get
+            {
+                return v.OKoltseg;
+            }
+        }
+        public double AElotti
+        {
+            get
+            {
+                return v.AElotti;
+            }
+        }
+        public double Ado
+        {
+            get
+            {
+                return v.Ado;
+            }
+        }
+        public double AEredmeny
+        {
+            get
+            {
+                return v.AEredmeny;
+            }
+        }
+    }
+}
