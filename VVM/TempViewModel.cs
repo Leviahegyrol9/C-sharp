5b86edb3bf5c4a009318d6a2829226f10b187977
Leviahegyrol69 <paplukacs.levi@gmail.com>
2026-05-22 13:27:58 +0200

    2026.05.22

---
+++ VVM/TempViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace VVM
+{
+    public partial class TempViewModel:ObservableObject
+    {
+        [ObservableProperty]
+        public string celsius;
+        [ObservableProperty]
+        public string fahrenheit = "Fahrenheit: ";
+        [ObservableProperty]
+        public string kelvin = "Kelvin: ";
+
+        [RelayCommand]
+        public void Converter()
+        {
+            double c = double.Parse(Celsius);
+            double f = (c * 9 /5) + 32;
+            double k = c + 273.15;
+
+            Fahrenheit = $"Fahrenheit: {f:n2} F";
+            Kelvin = $"Kelvin: {k:n2} K";
+        }
+
+        [RelayCommand]
+        public void Clear()
+        {
+            Celsius = string.Empty;
+            Fahrenheit = "Fahrenheit: ";
+            Kelvin = "Kelvin: ";
+        }
+    }
+}
---
+++ VVM2/PasswordViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommunityToolkit.Mvvm.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace VVM3
+{
+    public partial class PasswordViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string pwd;
+        [ObservableProperty]
+        private string passwordLength = "Hossz: ";
+        [ObservableProperty]
+        private string containsNumber = "Van szám?";
+        [ObservableProperty]
+        private string containsUppercase = "Van nagybetű?";
+        [ObservableProperty]
+        private string passwordStrength = "Erősség: ";
+
+        [RelayCommand]
+        public void Analyze()
+        {
+            PasswordLength = $"Hossz: {Pwd.Length}";
+            ContainsNumber = $"Van szám? {(Pwd.Any(char.IsDigit) ? "Igen" : "Nem")}";
+            ContainsUppercase = $"Van nagybetű? {(Pwd.Any(char.IsUpper) ? "Igen" : "Nem")}";
+
+            if (Pwd.Length < 6)
+            {
+                PasswordStrength = "Erősség: gyenge";
+            }
+            else if (Pwd.Length < 10)
+            {
+                PasswordStrength = "Erősség: közepes";
+            }
+            else PasswordStrength = "Erősség: erős";
+        }
+
+        [RelayCommand]
+        public void Clear()
+        {
+            Pwd = string.Empty;
+            PasswordLength = "Hossz: ";
+            ContainsNumber = "Van szám?";
+            ContainsUppercase = "Van nagybetű?";
+            PasswordStrength = "Erősség: ";
+        }
+    }
+}
